ed80ecd357449772ef7782edd0c807f4dd8d1161
Eduardo Müller Candaten <149803596+eduardocandaten@users.noreply.github.com>
2025-10-23 15:18:07 -0300

    copy_photos_to_destin.py

---
+++ src/utils/copy_photos_to_destin.py
@@ -0,0 +1,10 @@
+import shutil
+from pathlib import Path
+
+def copy_photos_to_destin(photos: list[Path], destin: Path):
+    destin.mkdir(parents=True, exist_ok=True)
+
+    for photo in photos:
+        destin_path = destin / photo.name
+        if not destin_path.exists():
+            shutil.copy2(photo, destin_path)
